0cc03e04756a0828ec5466317d4ae7b3d284c71b
fakorede <abiolafakorede@gmail.com>
2019-06-02 21:59:20 +0100

    implemented typed.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".type", {
+  strings: [
+    "JavaScript Developer",
+    "Fullstack Developer",
+    "Backend Developer",
+    "PHP Developer"
+  ],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop: true
+});
