932be1505b5b2afeb5a7dd5c9541be3309e21977
fakorede <abiolafakorede@gmail.com>
2019-06-03 15:50:15 +0100

    menu overlay and lots of adjustments

--- dist/js/loader.js
+++ dist/js/loader.js
@@ -1,14 +1,17 @@
 const loader = document.querySelector(".loader");
-const main = document.querySelector(".main");
+const header = document.querySelector(".header");
+const content = document.querySelector(".main-content");
 
 function init() {
   setTimeout(() => {
     loader.style.opacity = 0;
     loader.style.display = "none";
 
-    main.style.display = "block";
+    header.style.display = "block";
+    content.style.display = "block";
     setTimeout(() => {
-      main.style.opacity = 1;
+      header.style.opacity = 1;
+      content.style.opacity = 1;
     }, 50);
   }, 4000);
 }
